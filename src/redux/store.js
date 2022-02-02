ed191fe552987ed8647636db61cfecb6b5a3fdb8
Christopher T Wilson <vyper374@hotmail.com>
2022-02-02 12:36:09 +0000

    Added middleware to save and load state to localStorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,29 @@ import { configureStore } from '@reduxjs/toolkit'
 import userReducer from './userSlice'
 import cartReducer from './cartSlice';
 
+
+const localStorageMiddleware = ({getState}) => next => action => {
+	const result = next(action);
+
+	let blacklist = ['user/setUser'];
+	if (blacklist.includes(action.type)) return result;
+
+	localStorage.setItem('gameCommerceState', JSON.stringify(getState()));
+	return result;
+};
+
+const reHydrateStore = () => {
+	if (localStorage.getItem('gameCommerceState') !== null) {
+		return JSON.parse(localStorage.getItem('gameCommerceState'));
+	}
+};
+
 export const store = configureStore({
-  reducer: {
-      user: userReducer,
-      cart: cartReducer,
-  },
+	reducer: {
+		user: userReducer,
+		cart: cartReducer,
+	},
+	preloadedState: reHydrateStore(),
+  	middleware: getDefaultMiddleware =>
+    	getDefaultMiddleware().concat(localStorageMiddleware),
 })
