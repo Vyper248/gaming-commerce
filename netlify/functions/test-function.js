77926330868ad477887a45439bc16cc74caccce3
Christopher T Wilson <vyper374@hotmail.com>
2022-11-01 16:35:52 +0000

    Update to serverless function test

--- netlify/functions/test-function.js
+++ netlify/functions/test-function.js
@@ -4,14 +4,17 @@ exports.handler = async (event) => {
     let body = JSON.parse(event.body);
 
     if (body.readData) {
-      let dataFile = require('/tmp/data.json');
+      let dataFileJson = fs.readFileSync('/tmp/data.json');
+      let dataFile = dataFileJson ? JSON.parse(dataFileJson) : {};
 
       return {
           statusCode: 200,
           body: JSON.stringify(dataFile)
       }
     } else if (body.saveData) {
-      let dataFile = require('/tmp/data.json');
+      let dataFileJson = fs.readFileSync('/tmp/data.json');
+      let dataFile = dataFileJson ? JSON.parse(dataFileJson) : {};
+      
       let randomID = Math.round(Math.random()*10000);
       dataFile[randomID] = 'Saved';
 
